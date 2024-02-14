8aef1d81af031d361e96facb89ef5676131228fa
Lucas Ricardo <lucasricardobarb@gmail.com>
2024-02-14 20:40:28 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,20 @@
+// import { ThemeProvider } from 'styled-components'
+// import { BrowserRouter } from 'react-router-dom'
+
+// import { defaultTheme } from './styles/themes/default'
+// import { GlobalStyle } from './styles/global'
+// import { Router } from './Router'
+import { Home } from "./Home";
+
+export function App() {
+  return <Home />;
+
+  // return (
+  //   <ThemeProvider theme={defaultTheme}>
+  //     <BrowserRouter>
+  //       <Router />
+  //     </BrowserRouter>
+  //     <GlobalStyle />
+  //   </ThemeProvider>
+  // )
+}
---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,98 @@
+import { HandPalm, Play } from 'phosphor-react'
+import { useForm } from 'react-hook-form'
+import { zodResolver } from '@hookform/resolvers/zod'
+import * as zod from 'zod'
+import { useEffect, useState } from 'react'
+import { differenceInSeconds } from 'date-fns'
+
+import {
+  HomeContainer,
+  StartCountdownButton,
+  StopCountdownButton,
+} from './styles'
+import { NewCycleForm } from './components/NewCycleForm'
+import { Countdown } from './components/Countdown'
+
+interface Cycle {
+  id: string
+  task: string
+  minutesAmount: number
+  startDate: Date
+  interruptedDate?: Date
+  finishedDate?: Date
+}
+
+export function Home() {
+  const [cycles, setCycles] = useState<Cycle[]>([])
+  const [activeCycleId, setActiveCycleId] = useState<string | null>(null)
+
+  const activeCycle = cycles.find((cycle) => cycle.id === activeCycleId)
+
+  function handleCreateNewCicle(data: NewCycleFormData) {
+    const id = String(new Date().getTime())
+
+    const newCycle: Cycle = {
+      id,
+      task: data.task,
+      minutesAmount: data.minutesAmount,
+      startDate: new Date(),
+    }
+
+    setCycles((state) => [...state, newCycle])
+    setActiveCycleId(id)
+    setAmountSecondsPassed(0)
+
+    reset()
+  }
+
+  function handleInterruptCycle() {
+    setCycles((state) =>
+      state.map((cycle) => {
+        if (cycle.id === activeCycleId) {
+          return { ...cycle, interruptedDate: new Date() }
+        } else {
+          return cycle
+        }
+      }),
+    )
+    setActiveCycleId(null)
+  }
+
+  const currentSeconds = activeCycle ? totalSeconds - amountSecondsPassed : 0
+
+  const minutesAmont = Math.floor(currentSeconds / 60)
+  const secondsAmount = currentSeconds % 60
+
+  const minutes = String(minutesAmont).padStart(2, '0')
+  const seconds = String(secondsAmount).padStart(2, '0')
+
+  useEffect(() => {
+    if (activeCycle) {
+      document.title = `${minutes}:${seconds}`
+    }
+  }, [minutes, seconds, activeCycle])
+
+  const task = watch('task')
+  const isSubmitDisabled = !task
+
+  return (
+    <HomeContainer>
+      <form onSubmit={handleSubmit(handleCreateNewCicle)} action="">
+        <NewCycleForm />
+        <Countdown activeCycle={activeCycle} setCycles={setCycles} />
+
+        {activeCycle ? (
+          <StopCountdownButton onClick={handleInterruptCycle} type="button">
+            <HandPalm size={24} />
+            Interromper
+          </StopCountdownButton>
+        ) : (
+          <StartCountdownButton disabled={isSubmitDisabled} type="submit">
+            <Play size={24} />
+            Começar
+          </StartCountdownButton>
+        )}
+      </form>
+    </HomeContainer>
+  )
+}
